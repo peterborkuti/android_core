17425e5181dbd231a034cb39fcb2931572d4f5fa
Peter Borkuti <peter@aspire>
2016-05-15 12:52:48 +0200

    source format

--- android_serial_pubsub/src/org/ros/android/android_serial_pubsub/MainActivity.java
+++ android_serial_pubsub/src/org/ros/android/android_serial_pubsub/MainActivity.java
@@ -47,178 +47,180 @@ import java.util.Set;
  */
 public class MainActivity extends RosActivity {
 
-  /** USB **/
+    /*
+      * Notifications from UsbService will be received here.
+      */
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB: // NO USB CONNECTED
+                    Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+    private UsbService usbService;
+    private TextView display;
+    private EditText editText;
+    private MyHandler mHandler;
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+            usbService.setHandler(mHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
+    /**
+     * USBEND
+     **/
+    private RosTextView<std_msgs.String> rosTextView;
+    private Talker talker;
+
+    public MainActivity() {
+        // The RosActivity constructor configures the notification title and ticker
+        // messages.
+        super("Serial Pubsub Tutorial", "Serial Pubsub Tutorial");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setFilters();  // Start listening notifications from UsbService
+        startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
+    }
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        rosTextView = (RosTextView<std_msgs.String>) findViewById(R.id.text);
+        rosTextView.setTopicName("chatter");
+        rosTextView.setMessageType(std_msgs.String._TYPE);
+        rosTextView.setMessageToStringCallable(new MessageCallable<String, std_msgs.String>() {
+            @Override
+            public String call(std_msgs.String message) {
+                return message.getData();
+            }
+        });
+
+        // USB
+        mHandler = new MyHandler(this);
+        display = (TextView) findViewById(R.id.textView1);
+        editText = (EditText) findViewById(R.id.editText1);
+        Button sendButton = (Button) findViewById(R.id.buttonSend);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().equals("")) {
+                    String data = editText.getText().toString() + "\n";
+                    if (usbService != null) { // if UsbService was correctly binded, Send data
+                        usbService.write(data.getBytes());
+                    }
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+        talker = new Talker();
+
+        // At this point, the user has already been prompted to either enter the URI
+        // of a master to use or to start a master locally.
+
+        // The user can easily use the selected ROS Hostname in the master chooser
+        // activity.
+        NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(getRosHostname());
+        nodeConfiguration.setMasterUri(getMasterUri());
+        nodeMainExecutor.execute(talker, nodeConfiguration);
+        // The RosTextView is also a NodeMain that must be executed in order to
+        // start displaying incoming messages.
+        nodeMainExecutor.execute(rosTextView, nodeConfiguration);
+    }
+
+    /**
+     * USB
+     **/
 
 
     /*
      * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
      */
-  private static class MyHandler extends Handler {
-    private final WeakReference<MainActivity> mActivity;
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
 
-    public MyHandler(MainActivity activity) {
-      mActivity = new WeakReference<>(activity);
-    }
-
-    @Override
-    public void handleMessage(Message msg) {
-      switch (msg.what) {
-        case UsbService.MESSAGE_FROM_SERIAL_PORT:
-          String data = (String) msg.obj;
-          mActivity.get().display.append(data);
-          break;
-        case UsbService.CTS_CHANGE:
-          Toast.makeText(mActivity.get(), "CTS_CHANGE",Toast.LENGTH_LONG).show();
-          break;
-        case UsbService.DSR_CHANGE:
-          Toast.makeText(mActivity.get(), "DSR_CHANGE",Toast.LENGTH_LONG).show();
-          break;
-      }
-    }
-  }
-   /*
-     * Notifications from UsbService will be received here.
-     */
-  private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-      switch (intent.getAction()) {
-        case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
-          Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
-          break;
-        case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
-          Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
-          break;
-        case UsbService.ACTION_NO_USB: // NO USB CONNECTED
-          Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
-          break;
-        case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
-          Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
-          break;
-        case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
-          Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
-          break;
-      }
-    }
-  };
-  private UsbService usbService;
-  private TextView display;
-  private EditText editText;
-  private MyHandler mHandler;
-  private final ServiceConnection usbConnection = new ServiceConnection() {
-    @Override
-    public void onServiceConnected(ComponentName arg0, IBinder arg1) {
-      usbService = ((UsbService.UsbBinder) arg1).getService();
-      usbService.setHandler(mHandler);
-    }
-
-    @Override
-    public void onServiceDisconnected(ComponentName arg0) {
-      usbService = null;
-    }
-  };
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    setFilters();  // Start listening notifications from UsbService
-    startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
-  }
-  @Override
-  public void onPause() {
-    super.onPause();
-    unregisterReceiver(mUsbReceiver);
-    unbindService(usbConnection);
-  }
-
-  private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
-    if (!UsbService.SERVICE_CONNECTED) {
-      Intent startService = new Intent(this, service);
-      if (extras != null && !extras.isEmpty()) {
-        Set<String> keys = extras.keySet();
-        for (String key : keys) {
-          String extra = extras.getString(key);
-          startService.putExtra(key, extra);
+        public MyHandler(MainActivity activity) {
+            mActivity = new WeakReference<>(activity);
         }
-      }
-      startService(startService);
-    }
-    Intent bindingIntent = new Intent(this, service);
-    bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
-  }
 
-
-  private void setFilters() {
-    IntentFilter filter = new IntentFilter();
-    filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
-    filter.addAction(UsbService.ACTION_NO_USB);
-    filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
-    filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
-    filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
-    registerReceiver(mUsbReceiver, filter);
-  }
-
-
-
-  /** USBEND **/
-  private RosTextView<std_msgs.String> rosTextView;
-  private Talker talker;
-
-  public MainActivity() {
-    // The RosActivity constructor configures the notification title and ticker
-    // messages.
-    super("Serial Pubsub Tutorial", "Serial Pubsub Tutorial");
-  }
-
-  @SuppressWarnings("unchecked")
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    rosTextView = (RosTextView<std_msgs.String>) findViewById(R.id.text);
-    rosTextView.setTopicName("chatter");
-    rosTextView.setMessageType(std_msgs.String._TYPE);
-    rosTextView.setMessageToStringCallable(new MessageCallable<String, std_msgs.String>() {
-      @Override
-      public String call(std_msgs.String message) {
-        return message.getData();
-      }
-    });
-
-    // USB
-    mHandler = new MyHandler(this);
-    display = (TextView) findViewById(R.id.textView1);
-    editText = (EditText) findViewById(R.id.editText1);
-    Button sendButton = (Button) findViewById(R.id.buttonSend);
-    sendButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (!editText.getText().toString().equals("")) {
-          String data = editText.getText().toString() + "\n";
-          if (usbService != null) { // if UsbService was correctly binded, Send data
-            usbService.write(data.getBytes());
-          }
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UsbService.MESSAGE_FROM_SERIAL_PORT:
+                    String data = (String) msg.obj;
+                    mActivity.get().display.append(data);
+                    break;
+                case UsbService.CTS_CHANGE:
+                    Toast.makeText(mActivity.get(), "CTS_CHANGE", Toast.LENGTH_LONG).show();
+                    break;
+                case UsbService.DSR_CHANGE:
+                    Toast.makeText(mActivity.get(), "DSR_CHANGE", Toast.LENGTH_LONG).show();
+                    break;
+            }
         }
-      }
-    });
-
-  }
-
-  @Override
-  protected void init(NodeMainExecutor nodeMainExecutor) {
-    talker = new Talker();
-
-    // At this point, the user has already been prompted to either enter the URI
-    // of a master to use or to start a master locally.
-
-    // The user can easily use the selected ROS Hostname in the master chooser
-    // activity.
-    NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(getRosHostname());
-    nodeConfiguration.setMasterUri(getMasterUri());
-    nodeMainExecutor.execute(talker, nodeConfiguration);
-    // The RosTextView is also a NodeMain that must be executed in order to
-    // start displaying incoming messages.
-    nodeMainExecutor.execute(rosTextView, nodeConfiguration);
-  }
+    }
 }
